bafba217970dc40201c016526ecc997dde6ff5bf
Tim <tim.garret@inseec-france.com>
2017-10-10 16:52:30 +0200

    version jusqu'à exercice 3

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,23 @@
-#write your code here
+def add (a,b)
+  a + b
+end
+
+def subtract (a,b)
+  a - b
+end
+
+def sum (array)
+  sum_array = 0
+  array.each do |c|
+    sum_array += c
+  end
+  sum_array
+end
+
+def multiply (array)
+  multiply_array = 0
+  array.each do |c|
+    multiply_array *= c
+    end
+    multiply_array
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,19 @@
-#write your code here
+def echo (a)
+  "#{a}"
+end
+
+def shout (a)
+  "#{a.upcase}"
+end
+
+def repeat (a,b=(boucle=true;nil))
+  if boucle
+    "#{a} #{a}"
+  else
+    "#{a} " * (b-1) + "#{a}"
+end
+end
+def first_word (beg)
+  array = beg.split(' ')
+  array = 0
+end
